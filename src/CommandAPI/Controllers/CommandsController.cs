744b63c1f23ac611b459c6cf18cb32fba22b87db
muhammedogur88 <102020257+mogur88@users.noreply.github.com>
2022-04-30 23:48:34 +0300

    Initial commit

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CommandsController : ControllerBase
+{
+
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+        return new string[] { "This", "is", "hard", "coded" };
+    }
+}
---
+++ test/CommandAPI.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace CommandAPI.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
