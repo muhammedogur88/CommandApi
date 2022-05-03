1f0e2207acead78be3a9dfaf0afc313aee4d20c9
muhammedogur88 <102020257+mogur88@users.noreply.github.com>
2022-05-03 16:40:18 +0300

    Unit Tests are added

--- test/CommandAPI.Tests/CommandTests.cs
+++ test/CommandAPI.Tests/CommandTests.cs
@@ -4,21 +4,48 @@ using CommandAPI.Models;
 
 namespace CommandAPI.Tests;
 
-public class CommandTests
+public class CommandTests : IDisposable
 {
+    Command testCommand;
+    public CommandTests()
+    {
+        testCommand = new Command
+        {
+            HowTo = "Do something",
+            Platform = "Some platform",
+            CommandLine = "Some commandline"
+        };
+    }
+    public void Dispose()
+    {
+        testCommand = null;
+    }
     [Fact]
     public void CanChangeHowTo()
     {
         //Arrange
-        var testCommand = new Command
-        {
-            HowTo = "Do something awesome",
-            Platform = "xUnit",
-            CommandLine = "dotnet test"
-        };
         //Act
         testCommand.HowTo = "Execute Unit Tests";
+
         //Assert
         Assert.Equal("Execute Unit Tests", testCommand.HowTo);
+    }
+    [Fact]
+    public void CanChangePlatform()
+    {
+        //Arrange
+        //Act
+        testCommand.Platform = "xUnit";
+        //Assert
+        Assert.Equal("xUnit", testCommand.Platform);
+    }
+    [Fact]
+    public void CanChangeCommandLine()
+    {
+        //Arrange
+        //Act
+        testCommand.CommandLine = "dotnet test";
+        //Assert
+        Assert.Equal("dotnet test", testCommand.CommandLine);
     }
 }
